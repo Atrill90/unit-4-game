bee864122e09a82f18a47fc07bc9205a0e875c39
Alex Trilling <atrill90@gmail.com>
2018-04-01 22:25:07 -0400

    can't get the logic to go from first attack to second attack

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # unit-4-game
+
+* When the game begins choose a character!
+
+* Now pick an opponent you'd like to face.
+
+* Click the mushroom to attack your foe.
+
+* If you manage to kill your foe select a new opponent.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,60 +2,42 @@ $(document).ready(function() {
 
 
 // Mario is all arounder
-var mario={
+const mario={
     health:110,
     counter:14,
-    damage: function(){
-        return Math.floor(Math.random()*25)
+    damage: 19
     }
     
 
-}
-// Luigi can crit on number 7!
-var luigi={
+
+// Luigi 
+const luigi={
     health:100,
     counter:15,
-    damage: function(){
-        var randNum = Math.floor(Math.random()*10);
-        if (randNum===7){
-        return randNum * 10;
-        }else {
-            return randNum;
-        }
+    damage:20
 
-    }
+    
 }
-var Bowser={
+const bowser={
     health:200,
     counter:10,
-    damage: function(){
-        return Math.floor(Math.random()*15);
-
-    }
+    damage:14
+    
 }
 
-var Peach={
+const peach={
     health:125,
-    damage: function(){
-        return Math.floor(Math.random()*20);
-    }
-}
-var userChar = {
-    health:"",
-    attack:"",
-    counter:""
-
-}
-var enemyChar ={
-    health:"",
-    attack:"",
-    counter:""
+    counter:10,
+    damage: 15
 }
 
+const characterStats={
+    attCurrHealth:0,
+    defCurrHealth:0,
+    attackerDamage:null
+    
+}
 
-var enemyChar = false;
-
-var playerOne = false;
 
 // I need to be able to select a character click
 $(".cards").on("click",'.marioImage',function (e) {
@@ -78,20 +60,121 @@ $("#enemy").on("click", '.marioImage', function(){
     $("#enemy").hide();
     $(".hitspot").append("<img class='mushroomBtn img-fluid'>");
     $(".mushroomBtn").attr("src", "assets/images/mushroom.png")
-    .click(function(){
-     var user =   $('.marioImage:nth-child(1)');
-     var enemy = $('.marioImage:nth-child(2)');
-    console.log(enemy);
-
-    })
-
-
     
 
-}); 
+    })
+    $(".row").on("click",'.mushroomBtn', function(e){
+        var attackerId = e.currentTarget.parentElement.previousElementSibling.children[0].id ;
+        var defenderId =e.currentTarget.parentElement.nextElementSibling.children[0].id;
+        //first attack
+        var attacker = setAttackerStats(attackerId);
+        var defender = setDefenderStats(defenderId);
 
-// Each character is an object
+            defCurrHealth = defender.health - attacker.damage;
+            attCurrHealth = attacker.health - defender.counter;
+            if(!characterStats.attackerDamage){
+                displayStats(defCurrHealth,attCurrHealth);
+                characterStats.attCurrHealth = attCurrHealth;
+                characterStats.defCurrHealth = defCurrHealth;
+                 
+                attack(attacker, defender);
+                
+            }
+              
+              
+    });
 
+    const attack = function(attacker, defender){
+        // console.log(attacker, defender);
+        characterStats.attackerDamage = attacker.damage;
+        console.log(characterStats.attackerDamage);
+        if(characterStats.attackerDamage){
+            let twoXdamage = attacker.damage*2;
+            
+            characterStats.defCurrHealth = characterStats.defCurrHealth - twoXdamage;
+            characterStats.attCurrHealth = characterStats.attCurrHealth - defender.counter;
+            
+            displayStats(characterStats.defCurrHealth,characterStats.attCurrHealth);
+        }
+            
+
+    }
+        function setAttackerStats(attackerId) {
+            
+            switch(attackerId) {
+                case "mario":
+                    return {
+                        health: mario.health,
+                        counter: mario.counter,
+                        damage: mario.damage
+                    }
+                    break;
+                case "luigi":
+                return {
+                    health: luigi.health,
+                    counter: luigi.counter,
+                    damage: luigi.damage
+                }
+                    break;
+                case "bowser":
+                return {
+                    health: bowser.health,
+                    counter: bowser.counter,
+                    damage: bowser.damage
+                }
+                    break;
+                case "peach":
+                return {
+                    health: peach.health,
+                    counter: peach.counter,
+                    damage: peach.damage
+                }
+                    break;
+            }
+        }
+          
+
+
+        function setDefenderStats(defenderId){
+            switch(defenderId) {
+                case "mario":
+                    return {
+                        health: mario.health,
+                        counter: mario.counter,
+                        damage: mario.damage
+                    }
+                    break;
+                case "luigi":
+                return {
+                    health: luigi.health,
+                    counter: luigi.counter,
+                    damage: luigi.damage
+                }
+                    break;
+                case "bowser":
+                return {
+                    health: bowser.health,
+                    counter: bowser.counter,
+                    damage: bowser.damage
+                }
+                    break;
+                case "peach":
+                return {
+                    health: peach.health,
+                    counter: peach.counter,
+                    damage: peach.damage
+                }
+                    break;
+            }
+        }
+        
+        
+
+
+        function displayStats(defCurrHealth,attCurrHealth){
+            $(".defensestats").html(`<h2 class="text-center">Defenders Health</h2><br><h3 class="text-center">${defCurrHealth}</h3>`);
+            $(".attackstats").html(`<h2 class="text-center">Attackers Health</h2><br><h3 class="text-center">${attCurrHealth}</h3>`);
+        }
 // Health, Damage, Name 
 
 
